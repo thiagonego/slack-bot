475f87c38378ff0baf41ce466ce2806a828fc270
Thiago Sampaio <thiagonego@gmail.com>
2016-08-17 21:21:39 -0300

    token

--- src/main/java/us/sampaio/slack/controller/SlackSlashCommand.java
+++ src/main/java/us/sampaio/slack/controller/SlackSlashCommand.java
@@ -23,35 +23,17 @@ public class SlackSlashCommand {
 
     private static final Logger logger = LoggerFactory.getLogger(SlackSlashCommand.class);
 
-    /**
-     * The token you get while creating a new Slash Command. You
-     * should paste the token in application.properties file.
-     */
     @Value("${slashcommandtoken}")
     private String slackToken;
+    
+    @Value("${usuarios}")
+    private String usuarios;
     
     @RequestMapping(value = "/echo", method = RequestMethod.GET)
     public String echo(){
          return String.format("ECHO em: %s", Data.getDataFormatada(new Date(), "dd/MM/yyyy HH:mm:ss.SSS"));
     }
 
-    /**
-     * Slash Command handler. When a user types for example "/app help"
-     * then slack sends a POST request to this endpoint. So, this endpoint
-     * should match the url you set while creating the Slack Slash Command.
-     *
-     * @param token
-     * @param teamId
-     * @param teamDomain
-     * @param channelId
-     * @param channelName
-     * @param userId
-     * @param userName
-     * @param command
-     * @param text
-     * @param responseUrl
-     * @return
-     */
     @RequestMapping(value = "/slash-command",
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
@@ -65,8 +47,9 @@ public class SlackSlashCommand {
                                              @RequestParam("command") String command,
                                              @RequestParam("text") String text,
                                              @RequestParam("response_url") String responseUrl) {
+        
         if (!token.equals(slackToken)) {
-            return new RichMessage("Sorry! You're not lucky enough to use our slack command.");
+            return new RichMessage("Desculpe! Você não tem permissão para utilizar nosso bot.");
         }
 
         RichMessage richMessage = new RichMessage("The is Slash Commander!");
